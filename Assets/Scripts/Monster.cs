dd43f49fe0ad09d6bbe7ac9ecdb3aa36f13ea692
mingliang.liu <54487356@qq.com>
2017-08-25 21:49:01 +0800

    no message

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -29,15 +29,13 @@ public class Monster : MonoBehaviour {
 	
 	float radian = 0; // 弧度  
 	float perRadian = 0.03f; // 每次变化的弧度  
-	float radius = 0.001f; // 半径  
+	float radius = 0.002f; // 半径  
 
 	// Update is called once per frame  
 	void Update () {
 		radian += perRadian; // 弧度每次加0.03  
 		float dy = Mathf.Cos(radian) * radius; // dy定义的是针对y轴的变量，也可以使用sin，找到一个适合的值就可以  
-		this.transform.position = this.transform. position  + new Vector3 (dy, 0, 0);  
-
-
+		this.transform.position = this.transform. position  + new Vector3 (dy, 0, 0); 
 	}
 
 
